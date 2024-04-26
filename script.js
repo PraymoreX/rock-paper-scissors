eee970c1ac1c3371ec27ea8191dc6787ad1c4a80
PraymoreX <omenipraymore18@gmail.com>
2024-04-26 15:14:16 +0000

    Use DOM to hide and display text and button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+/*function getComputerChoice() {
     let ComputerNum = Math.floor(Math.random() * 3); 
     if (ComputerNum === 0) {
       return 'rock'
@@ -10,7 +10,7 @@ function getComputerChoice() {
       return 'scissors'
     }
    };
-   let computerChoice = getComputerChoice();
+   */
    
 
    let btn = document.querySelectorAll("button");
@@ -26,30 +26,57 @@ function getComputerChoice() {
    let results = document.querySelector("h4");
    let declaration = document.querySelector("h5");
 
-
-   //loop scores until either reaches 5
+   let resultstext = document.querySelector("h3");
+ 
 
    let playerScore = 0;
    let computerScore = 0;
 
-   if (playerScore === 5) {
-    declaration.textContent = 'Congrats !! You are the winner !';
-  } else if (computerScore === 5) {
-    declaration.textContent = 'Oh no you lost...'
-  };
+   newgame.style.display = "none";
 
+   
 
-
-    
-   getComputerChoice();
+   
    rockbtn.addEventListener("click", ifRock);
+   
+   
 
    function ifRock() {
+
+    function getComputerChoice() {
+      let ComputerNum = Math.floor(Math.random() * 3); 
+      if (ComputerNum === 0) {
+        return 'rock'
+      } 
+      else if (ComputerNum === 1) {
+        return 'paper'
+      }
+      else if (ComputerNum === 2) {
+        return 'scissors'
+      }
+     };
+     let computerChoice = getComputerChoice();
+
     
     if(computerChoice === "rock"){
       playerScoreText.textContent = `Player Score: ${playerScore}`;
       computerScoreText.textContent = `Computer Score: ${computerScore}`;
       results.textContent = "Computer chose Rock,  It's a tie!";
+      if (playerScore === 5) {
+        declaration.textContent = 'Congrats !! You are the winner !';
+      } else if (computerScore === 5) {
+        declaration.textContent = 'Oh no you lost...'
+      };
+      if (playerScore === 5 || computerScore === 5) {
+        playerScoreText.style.display = "none";
+        computerScoreText.style.display = "none";
+        rockbtn.style.display = "none";
+        paperbtn.style.display = "none";
+        scissorsbtn.style.display = "none";
+        newgame.style.display = "block";
+        resultstext.textContent = "Final Results:";
+      
+    }
         
 
     }
@@ -58,6 +85,21 @@ function getComputerChoice() {
         playerScoreText.textContent = `Player Score: ${playerScore}`;
         computerScoreText.textContent = `Computer Score: ${computerScore}`;
         results.textContent = "Computer chose Paper,  You lost!";
+        if (playerScore === 5) {
+          declaration.textContent = 'Congrats !! You are the winner !';
+        } else if (computerScore === 5) {
+          declaration.textContent = 'Oh no you lost...'
+        };
+        if (playerScore === 5 || computerScore === 5) {
+          playerScoreText.style.display = "none";
+          computerScoreText.style.display = "none";
+          rockbtn.style.display = "none";
+          paperbtn.style.display = "none";
+          scissorsbtn.style.display = "none";
+          newgame.style.display = "block";
+          resultstext.textContent = "Final Results:";
+        
+      }
     }
 
     else if(computerChoice === 'scissors'){
@@ -65,49 +107,135 @@ function getComputerChoice() {
       playerScoreText.textContent = `Player Score: ${playerScore}`;
       computerScoreText.textContent = `Computer Score: ${computerScore}`;
       results.textContent = "Computer chose Scissors,  You won!";
-      
+      if (playerScore === 5) {
+        declaration.textContent = 'Congrats !! You are the winner !';
+      } else if (computerScore === 5) {
+        declaration.textContent = 'Oh no you lost...'
+      };
+      if (playerScore === 5 || computerScore === 5) {
+        playerScoreText.style.display = "none";
+        computerScoreText.style.display = "none";
+        rockbtn.style.display = "none";
+        paperbtn.style.display = "none";
+        scissorsbtn.style.display = "none";
+        newgame.style.display = "block";
+        resultstext.textContent = "Final Results:";
       
     }
-    
-   };
+  }
+      
+    };
 
-   getComputerChoice();
+   
    
 
-   paperbtn.addEventListener("click", ifPaper);
+  paperbtn.addEventListener("click", ifPaper);
+   
    function ifPaper() {
+
+    function getComputerChoice() {
+      let ComputerNum = Math.floor(Math.random() * 3); 
+      if (ComputerNum === 0) {
+        return 'rock'
+      } 
+      else if (ComputerNum === 1) {
+        return 'paper'
+      }
+      else if (ComputerNum === 2) {
+        return 'scissors'
+      }
+     };
+     let computerChoice = getComputerChoice();
     
     if(computerChoice === "rock"){
       playerScore++;
       playerScoreText.textContent = `Player Score: ${playerScore}`;
       computerScoreText.textContent = `Computer Score: ${computerScore}`;
       results.textContent = "Computer chose Rock,  You win!";
+      if (playerScore === 5) {
+        declaration.textContent = 'Congrats !! You are the winner !';
+      } else if (computerScore === 5) {
+        declaration.textContent = 'Oh no you lost...'
+      };
+      if (playerScore === 5 || computerScore === 5) {
+        playerScoreText.style.display = "none";
+        computerScoreText.style.display = "none";
+        rockbtn.style.display = "none";
+        paperbtn.style.display = "none";
+        scissorsbtn.style.display = "none";
+        newgame.style.display = "block";
+        resultstext.textContent = "Final Results:";
+      
+    }
     }
 
     else if(computerChoice === 'paper'){
       playerScoreText.textContent = `Player Score: ${playerScore}`;
       computerScoreText.textContent = `Computer Score: ${computerScore}`;
       results.textContent = "Computer chose Paper,  It's a tie!";
+      if (playerScore === 5) {
+        declaration.textContent = 'Congrats !! You are the winner !';
+      } else if (computerScore === 5) {
+        declaration.textContent = 'Oh no you lost...'
+      };
+      if (playerScore === 5 || computerScore === 5) {
+        playerScoreText.style.display = "none";
+        computerScoreText.style.display = "none";
+        rockbtn.style.display = "none";
+        paperbtn.style.display = "none";
+        scissorsbtn.style.display = "none";
+        newgame.style.display = "block";
+        resultstext.textContent = "Final Results:";
+      
+    }
 
     }
+  
 
     else if (computerChoice === 'scissors'){
       computerScore++;
       playerScoreText.textContent = `Player Score: ${playerScore}`;
       computerScoreText.textContent = `Computer Score: ${computerScore}`;
       results.textContent  ="Computer chose Scissors,  You lost!";
-      
+      if (playerScore === 5) {
+        declaration.textContent = 'Congrats !! You are the winner !';
+      } else if (computerScore === 5) {
+        declaration.textContent = 'Oh no you lost...'
+      };
+      if (playerScore === 5 || computerScore === 5) {
+        playerScoreText.style.display = "none";
+        computerScoreText.style.display = "none";
+        rockbtn.style.display = "none";
+        paperbtn.style.display = "none";
+        scissorsbtn.style.display = "none";
+        newgame.style.display = "block";
+        resultstext.textContent = "Final Results:";
       
     }
+      
+  }
+    
     
    };
-
-   getComputerChoice();
    
-
-
+  
   scissorsbtn.addEventListener("click", ifScissors);
+
    function ifScissors() {
+
+    function getComputerChoice() {
+      let ComputerNum = Math.floor(Math.random() * 3); 
+      if (ComputerNum === 0) {
+        return 'rock'
+      } 
+      else if (ComputerNum === 1) {
+        return 'paper'
+      }
+      else if (ComputerNum === 2) {
+        return 'scissors'
+      }
+     };
+     let computerChoice = getComputerChoice();
     
     if(computerChoice === "rock"){
       computerScore++;
@@ -115,36 +243,73 @@ function getComputerChoice() {
       computerScoreText.textContent = `Computer Score: ${computerScore}`;
       results.textContent = "Computer chose Rock,  You lose!";
 
-      }
+      if (playerScore === 5) {
+        declaration.textContent = 'Congrats !! You are the winner !';
+      } else if (computerScore === 5) {
+        declaration.textContent = 'Oh no you lost...'
+      };
+      if (playerScore === 5 || computerScore === 5) {
+        playerScoreText.style.display = "none";
+        computerScoreText.style.display = "none";
+        rockbtn.style.display = "none";
+        paperbtn.style.display = "none";
+        scissorsbtn.style.display = "none";
+        newgame.style.display = "block";
+        resultstext.textContent = "Final Results:";
+      
+    }
+  }
+
+      
 
     else if(computerChoice === 'paper'){
       playerScore++;
       playerScoreText.textContent = `Player Score: ${playerScore}`;
       computerScoreText.textContent = `Computer Score: ${computerScore}`;
       results.textContent = "Computer chose Paper,  You win!";
+    
+      if (playerScore === 5) {
+        declaration.textContent = 'Congrats !! You are the winner !';
+      } else if (computerScore === 5) {
+        declaration.textContent = 'Oh no you lost...'
+      };
+      if (playerScore === 5 || computerScore === 5) {
+        playerScoreText.textContent.style.display = "none";
+        computerScoreText.style.display = "none";
+        rockbtn.style.display = "none";
+        paperbtn.style.display = "none";
+        scissorsbtn.style.display = "none";
+        newgame.style.display = "block";
+        resultstext.textContent = "Final Results:";
       
-      }
+    }
+  }
+      
+      
 
     else if (computerChoice === 'scissors'){
       playerScoreText.textContent = `Player Score: ${playerScore}`;
       computerScoreText.textContent = `Computer Score: ${computerScore}`;
       results.textContent =  "Computer chose Scissors,  It's a tie!";
+
+      if (playerScore === 5) {
+        declaration.textContent = 'Congrats !! You are the winner !';
+      } else if (computerScore === 5) {
+        declaration.textContent = 'Oh no you lost...'
+      };
+      if (playerScore === 5 || computerScore === 5) {
+        playerScoreText.style.display = "none";
+        computerScoreText.style.display = "none";
+        rockbtn.style.display = "none";
+        paperbtn.style.display = "none";
+        scissorsbtn.style.display = "none";
+        newgame.style.display = "block";
+        resultstext.textContent = "Final Results:";
       
     }
     
-  };
+  }
+};
    newgame.addEventListener("click", () => {
     location.reload();
    })
-
-   
-   /* Game Results
-if (playerScore === 5) {
-  alert('Congrats !! You are the winner !')
-} else if (computerScore === 5) {
-  alert('Oh no you lost...')
-// }*/
-   
-   
-
-
